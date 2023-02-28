51b9a1ea0794c79825422799cc2dbbd3c7f8741f
jsalatiel <jsalatiel@users.noreply.github.com>
2023-02-28 19:10:25 -0300

    Update bkp.sh
    fix typo

--- defaults/bkp.sh
+++ defaults/bkp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mdir -p /config/mysql-bkps/ &&  mysqldump -u root zm | gzip -c > /config/mysql-bkps/dump.$(date +%F).dump.gz
+mkdir -p /config/mysql-bkps/ &&  mysqldump -u root zm | gzip -c > /config/mysql-bkps/dump.$(date +%F).dump.gz
