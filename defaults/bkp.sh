b0c46f8eb1369ae6a152f094b4aa9a8b580c1bcc
jsalatiel <jsalatiel@users.noreply.github.com>
2023-03-01 21:46:38 -0300

    Update bkp.sh

--- defaults/bkp.sh
+++ defaults/bkp.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mkdir -p /config/mysql-bkps/ &&  mysqldump -u root zm | gzip -c > /config/mysql-bkps/dump.$(date +%F).dump.gz
+[ -f /etc/.do_mysql_backups ] || exit 0
+mkdir -p /config/mysql-bkps/
+find /config/mysql-bkps  -type f -ctime +60 -delete # keep backups for 2 months
+mysqldump -u root zm | nice -n19 bzip2 -c > /config/mysql-bkps/dump.$(date +%F).dump.bz2
